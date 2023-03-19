d7006aec61d31efe86d81fc1d6740eae40492df5
HoangAnh109 <127107895+HoangAnh109@users.noreply.github.com>
2023-03-19 07:27:03 +0700

    Add files via upload

--- SortAlgorithm.cpp
+++ SortAlgorithm.cpp
@@ -3,7 +3,8 @@
 #include <chrono>
 #include <vector>
 #include <fstream>
-
+#include <functional>
+#include <iomanip>
 using namespace std;
 
 // function to sort vector using heap sort
@@ -94,15 +95,31 @@ void quickSort(vector<double>& vec, int left, int right) {
         quickSort(vec, i, right);
 }
 
+// function to check if vector is sorted
+bool isSorted(vector<double> sorted, vector<double> check){
+    if (sorted == check) return true;
+    else return false;
+}
+
+// function to measure excution time of sort
+double measure_execution_time(std::function<void()> code_block) {
+    auto start_time = std::chrono::high_resolution_clock::now();
+    code_block();
+    auto end_time = std::chrono::high_resolution_clock::now();
+    double execution_time = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+    
+    return execution_time;
+}
+
 int main() {
     ofstream out_put("out_put.txt");
+
     vector<string> fileNames = {"Nums_Test01.txt", "Nums_Test02.txt",
                                 "Nums_Test03.txt", "Nums_Test04.txt",
                                 "Nums_Test05.txt", "Nums_Test06.txt",
                                 "Nums_Test07.txt", "Nums_Test08.txt",
                                 "Nums_Test09.txt", "Nums_Test10.txt"};
     
-
     for (const auto& fileName : fileNames) {
         ifstream file(fileName);
         if (file.is_open()) {
@@ -118,33 +135,48 @@ int main() {
             auto heapSortVec = vec;
             auto mergeSortVec = vec;
             auto quickSortVec = vec;
-            auto SortFunctionVec = vec;
+            
+            // Recording the time std::sort() sort vector
+            double time = measure_execution_time([&vec]{
+                sort(vec.begin(), vec.end());
+            }) ;
+            out_put << time <<"\t\t";
+            
+            // Recording the time heapsort sort vector
+            time = measure_execution_time([&heapSortVec]{
+                heapSort(heapSortVec);
+            }) ;
+            if (isSorted(vec, heapSortVec) &&  (int)time != 0)
+                out_put << (int)time <<"\t\t";
+            else {
+                // check if vector isn't sorted
+                cout << "Heapsort : " <<fileName << " isn't sorted\n";
+                out_put << "error" <<"\t\t";
+            }
 
-            auto start = chrono::high_resolution_clock::now();
+            // Recording the time mergesort sort vector
+            time = measure_execution_time([&mergeSortVec, n]{
+                mergeSort(mergeSortVec, 0, n - 1);
+            }) ;
+            if (isSorted(vec, mergeSortVec) && (int)time != 0)
+                out_put << (int)time <<"\t\t";
+            else {
+                // check if vector isn't sorted
+                cout << "Mergesort : " << fileName << " isn't sorted\n";
+                out_put << "error" <<"\t\t";
+            }
 
-            heapSort(heapSortVec);   
-                  
-            auto end = chrono::high_resolution_clock::now();
-            auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-            out_put << duration.count() << "     ";
-
-            start = chrono::high_resolution_clock::now();
-            mergeSort(mergeSortVec, 0, n - 1);
-            end = chrono::high_resolution_clock::now();
-            duration = chrono::duration_cast<chrono::microseconds>(end - start);
-            out_put  << duration.count() <<"     ";
-
-            start = chrono::high_resolution_clock::now();
-            quickSort(quickSortVec, 0, n - 1);
-            end = chrono::high_resolution_clock::now();
-            duration = chrono::duration_cast<chrono::microseconds>(end - start);
-            out_put  << duration.count() <<"     ";
-
-            start = chrono::high_resolution_clock::now();
-            sort(SortFunctionVec.begin(), SortFunctionVec.end());
-            end = chrono::high_resolution_clock::now();
-            duration = chrono::duration_cast<chrono::microseconds>(end - start);
-            out_put << duration.count() << endl;
+            // Recording the time quicksort sort vector
+            time = measure_execution_time([&quickSortVec, n]{
+                quickSort(quickSortVec, 0, n - 1);
+            }) ;
+            if (isSorted(vec, quickSortVec) && (int)time != 0)
+                out_put<< time <<"\n";
+            else {
+                // check if vector isn't sorted
+                cout << "Quicksort : " <<fileName << " isn't sorted\n";
+                out_put << "error" <<"\n";
+            }
 
             file.close();
         } 
